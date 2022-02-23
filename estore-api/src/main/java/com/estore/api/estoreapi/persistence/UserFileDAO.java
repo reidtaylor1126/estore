0b5d2ed54c38cffb523d508b88a2463454334db1
Reid Taylor <reidtaylor1126@gmail.com>
2022-02-22 21:28:57 -0500

    set up user account model and DAO classes, ready for CRUD implementation

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserFileDAO.java
@@ -0,0 +1,49 @@
+package com.estore.api.estoreapi.persistence;
+
+import java.io.*;
+import java.util.*;
+
+import com.estore.api.estoreapi.model.UserAccount;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFileDAO implements UserDAO{
+
+    /**
+     * The set of users
+     */
+    private Map<String, UserAccount> users;
+
+    /**
+     * The file name of the user data file
+     */
+    private String filename;
+
+    /**
+     * The object mapper.
+     */
+    private ObjectMapper objectMapper;
+
+    public UserFileDAO(@Value("${users.filename}") String filename, ObjectMapper objectMapper) throws IOException {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+        loadUsers();
+    }
+
+    private ArrayList<UserAccount> getUserList() {
+        return new ArrayList<>(users.values());
+    }
+
+    private void loadUsers() throws IOException{
+        users = new TreeMap<>();
+        UserAccount[] userArray = objectMapper.readValue(new File(filename), UserAccount[].class);
+        for(UserAccount account : userArray) {
+            users.put(account.getUsername(), account);
+        }
+    }
+
+    // CRUD methods need implementation
+}
