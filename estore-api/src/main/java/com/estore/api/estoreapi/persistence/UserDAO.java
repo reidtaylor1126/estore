77a91c19981c7ffa3abe6f5a7b4e14b6a40cc149
ctr7569 <ctr7569@rit.edu>
2022-03-09 17:20:27 -0500

    Created UserController and worked on methods for UserLogin, working on Session Management

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
@@ -1,7 +1,9 @@
 package com.estore.api.estoreapi.persistence;
 
+import com.estore.api.estoreapi.model.UserAccount;
+
 public interface UserDAO {
 
-    public UserAccount loginUser(string username, string password) throws IOException;
+    public UserAccount loginUser(String username, String password);
 
 }
