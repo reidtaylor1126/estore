a71805ab31a8c201b8e5e551c267256650eee1c6
ctr7569 <ctr7569@rit.edu>
2022-03-09 02:52:30 -0500

    Save point before going in and attempting to mess around with spring session

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/UserController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/UserController.java
@@ -21,3 +21,31 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+    private UserDAO userDAO;
+
+    public UserController(UserDAO _userDAO) {
+        this.userDAO = _userDAO;
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<Product[]> searchProduct(@RequestParam String q) {
+        LOG.info("GET /users/user?q=" + q);
+        try {
+            User user = userDAO.loginUser(q);
+            if (user != null) {
+                return new ResponseEntity<User>(user, HttpStatus.OK);
+            }
+            else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (IOException ioe) {
+            LOG.log(Level.SEVERE, ioe.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
