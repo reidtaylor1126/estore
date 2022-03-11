a4b1b9e1f94208d5a9978a9747719a78d0c7eff5
Nate Appleby <npa1508@rit.edu>
2022-03-10 23:25:44 -0500

    Finished steps for CreateUser: - UserController created - getId() added to UserAccount - header added to UserDAO - created saveUsers() in UserFileDAO - added id tracking in UserFileDAO - changed getUserList to getUserArray in UserFileDAO - added createUser() in UserFileDAO

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
@@ -1,5 +1,11 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.IOException;
+
+import com.estore.api.estoreapi.model.UserAccount;
+
 public interface UserDAO {
     // Needs method headers
+
+    public UserAccount createUser(UserAccount user) throws IOException;
 }
--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserFileDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserFileDAO.java
@@ -27,23 +27,59 @@ public class UserFileDAO implements UserDAO{
      */
     private ObjectMapper objectMapper;
 
+    private static int nextId;
+
     public UserFileDAO(@Value("${users.filename}") String filename, ObjectMapper objectMapper) throws IOException {
         this.filename = filename;
         this.objectMapper = objectMapper;
         loadUsers();
     }
 
-    private ArrayList<UserAccount> getUserList() {
-        return new ArrayList<>(users.values());
+    private synchronized static int nextId() {
+        int id = nextId;
+        ++nextId;
+        return id;
+    }
+
+    private UserAccount[] getUserArray() {
+        ArrayList<UserAccount> userArrayList = new ArrayList<>();
+
+        for (UserAccount user : users.values()) {
+            userArrayList.add(user);
+        }
+
+        UserAccount[] userArray = new UserAccount[userArrayList.size()];
+        userArrayList.toArray(userArray);
+        return userArray;
+    }
+
+    private void saveUsers() throws IOException {
+        UserAccount[] userArray = getUserArray();
+        objectMapper.writeValue(new File(filename),userArray);
     }
 
     private void loadUsers() throws IOException{
         users = new TreeMap<>();
+        nextId = 0; // May need to be changed to 1
         UserAccount[] userArray = objectMapper.readValue(new File(filename), UserAccount[].class);
         for(UserAccount account : userArray) {
             users.put(account.getUsername(), account);
+            if (account.getId() > nextId()) {
+                nextId = account.getId();
+            }
+            ++nextId;
         }
     }
 
     // CRUD methods need implementation
+
+    @Override
+    public UserAccount createUser(UserAccount user) throws IOException, IllegalArgumentException{
+        synchronized(users) {
+            UserAccount newUser = new UserAccount(nextId(), user.getUsername());
+            users.put(newUser.getUsername(), newUser);
+            saveUsers();
+            return newUser;
+        }
+    }
 }
