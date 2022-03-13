adaa7987afbc9c2a2a9465a12378d9825f5a4cf8
Ryan Yocum <rty4159@rit.edu>
2022-03-12 21:32:21 -0500

    Connected auth to backend

--- estore-ui/src/app/services/auth/auth.service.ts
+++ estore-ui/src/app/services/auth/auth.service.ts
@@ -15,35 +15,45 @@ export class AuthService {
     constructor(private httpClient: HttpClient) {}
 
     login(username: string): Observable<User> {
-        console.log('login');
-        return this.fakeUserAuth(username).pipe(
-            map((user) => {
-                this.currentUserSubject.next(user);
-                localStorage.setItem('currentUser', JSON.stringify(user));
-                return user;
+        return this.httpClient
+            .get(`/api/users/useraccount?q=${username}`, {
+                responseType: 'text',
             })
-        );
+            .pipe(
+                map((user) => {
+                    const userArr = user.split('*');
+                    const userObj = {
+                        username: userArr[0],
+                        admin: userArr[2] === 'true',
+                    };
+                    this.currentUserSubject.next(userObj);
+                    localStorage.setItem(
+                        'currentUser',
+                        JSON.stringify(userObj)
+                    );
+                    return userObj;
+                })
+            );
     }
 
-    register(username: string): Observable<User> {
-        return this.fakeUserAuth(username).pipe(
-            map((user) => {
-                this.currentUserSubject.next(user);
-                localStorage.setItem('currentUser', JSON.stringify(user));
-                return user;
-            })
-        );
-    }
+    // register(username: string): Observable<User> {
+    //     console.log('login');
+    //     return this.httpClient.get<string>(`/api/users?q=${username}`).pipe(
+    //         map((user) => {
+    //             const userArr = user.split('*');
+    //             const userObj = {
+    //                 username: userArr[0],
+    //                 admin: userArr[1] === 'true',
+    //             };
+    //             this.currentUserSubject.next(userObj);
+    //             localStorage.setItem('currentUser', JSON.stringify(user));
+    //             return userObj;
+    //         })
+    //     );
+    // }
 
     logout(): void {
         this.currentUserSubject.next(undefined);
         localStorage.removeItem('currentUser');
-    }
-
-    private fakeUserAuth(username: string): Observable<User> {
-        return of({
-            username,
-            admin: username === 'admin' ? true : false,
-        });
     }
 }
