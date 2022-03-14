3ffe86af8fd6c77344a567cc6cdab927178b563a
Reid Taylor <reidtaylor1126@gmail.com>
2022-03-14 15:29:50 -0400

    added code documentation

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
@@ -9,5 +9,12 @@ public interface UserDAO {
 
     public UserAccount loginUser(String username, String password);
 
+    /**
+     * Determines whether a token matches with an existing user account
+     * @param token The submitted token
+     * @return A {@link UserAccount} if one matching the token is found
+     * @throws AccountNotFoundException If a {@link UserAccount} is not found
+     * @throws InvalidTokenException If a {@link UserAccount} is found but the token is invalid
+     */
     public UserAccount verifyToken(String token) throws AccountNotFoundException, InvalidTokenException;
 }
