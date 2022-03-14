8e5bbef830cbfadefc34c4f542b95c29dcd0e0a2
Ryan Yocum <rty4159@rit.edu>
2022-03-14 14:39:49 -0400

    merged with user-accounts 2

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
@@ -1,16 +1,5 @@
 package com.estore.api.estoreapi.persistence;
 
-<<<<<<< HEAD
-import com.estore.api.estoreapi.model.UserAccount;
-import java.io.IOException;
-import com.estore.api.estoreapi.model.UserAccount;
-
-public interface UserDAO {
-
-    public UserAccount loginUser(String username);
-
-    public UserAccount createUser(UserAccount user) throws IOException;
-=======
 import java.io.IOException;
 
 import com.estore.api.estoreapi.model.UserAccount;
@@ -19,6 +8,6 @@ public interface UserDAO {
     // Needs method headers
 
     public UserAccount createUser(UserAccount user) throws IOException;
+
     public UserAccount loginUser(String username);
->>>>>>> origin/user-accounts
 }
